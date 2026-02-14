56d7d34d3e18bdfd1359dfa9e222467e440a33e9
bhushanpatil288 <sharewithbhushan@gmail.com>
2026-02-14 20:52:53 +0530

    [changes] - [header] - added responsiveness and changed selection color

--- src/components/main/section-hero/Hero.jsx
+++ src/components/main/section-hero/Hero.jsx
@@ -6,22 +6,22 @@ const Hero = () => {
   return (
     <div className="container mt-5 pt-4">
       <div className="row">
-          <div className="col-4">
+          <div className="col-lg-4">
             <div className="pe-5">
               <p className="pe-5 mt-5 heroPara position-relative">Recognized by industry leaders, of our award-winning team has a proven record of delivering excellence across projects</p>
             </div>
           </div>
-          <div className="col-8">
+          <div className="col-lg-8">
             <div className="d-flex flex-column gap-4">
               <h1 className="display-1 fw-bolder">Driving Innovation to Transform Business Futures <RiArrowRightUpLine size={68} /></h1>
-              <div className="row justify-content-center align-items-center">
-                <div className="col-3">
+              <div className="row justify-content-center align-items-center g-3 g-lg-0">
+                <div className="col-lg-3">
                   <ButtonCustom text="Get Started" />
                 </div>
-                <div className="col-6">
+                <div className="col-lg-6">
                   <p className="fs-5 m-0">Recognized by industryaward leaders, award winning team has be a proven record.</p>
                 </div>
-                <div className="col-3"></div>
+                <div className="col-0 col-lg-3"></div>
               </div>
             </div>
           </div>
